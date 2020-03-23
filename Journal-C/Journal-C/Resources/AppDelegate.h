b89e9cfb307a7877e693809b238666aa7f61b790
Anthroman <30359515+Anthroman@users.noreply.github.com>
2020-03-23 14:25:38 -0600

    Set up initial project folders

---
+++ Journal-C/Journal-C/Resources/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Journal-C
+//
+//  Created by Anthroman on 3/23/20.
+//  Copyright © 2020 Anthroman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ Journal-C/Journal-C/Resources/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  Journal-C
+//
+//  Created by Anthroman on 3/23/20.
+//  Copyright © 2020 Anthroman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
